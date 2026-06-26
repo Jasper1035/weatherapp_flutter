a98557285fdf1187e1136491b77f3fcd09e079ac
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-06-26 18:21:05 +0100

    add the api functions

--- weatherapp_flutter/lib/weather_service.dart
+++ weatherapp_flutter/lib/weather_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'package:weatherapp_flutter/weather_model.dart';
 
@@ -18,5 +20,26 @@ class WeatherService {
     } else {
       throw Exception('Failed to load weather data');
     }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    List<Placemark> placemark = await placemarkFromCoordinates(
+      position.latitude,
+      position.longitude,
+    );
+
+    String? city = placemark[0].locality;
+
+    return city ?? '';
   }
 }
--- weatherapp_flutter/lib/weatherpage.dart
+++ weatherapp_flutter/lib/weatherpage.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp_flutter/weather_model.dart';
+import 'package:weatherapp_flutter/weather_service.dart';
 
-class Weatherpage extends StatelessWidget {
+class Weatherpage extends StatefulWidget {
   const Weatherpage({super.key});
 
   @override
+  State<Weatherpage> createState() => _WeatherpageState();
+}
+
+class _WeatherpageState extends State<Weatherpage> {
+  final weatherService = WeatherService('');
+  WeatherModel? _weather;
+
+  _fetchWeather() async {
+    String cityName = await weatherService.getCurrentCity();
+    try {
+      final weather = await weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
